64a6c8f6bb896bb281494bd3b0ec867763b8606b
Daniel Eppler <DanielEppler51@gmail.com>
2024-12-13 09:29:09 +0200

    pakcage.json modifications

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 require('./axios')
 require('dotenv').config()
@@ -9,6 +10,12 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use(express.static(path.join(__dirname,'../front/dist')))
+
+ app.get('/app',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../front/dist/index.html'))
+ })
 
 const teamsController =  require('./controllers/teamsController')
 app.use('/teams',teamsController)
